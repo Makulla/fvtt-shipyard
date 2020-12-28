58e6cb2ac74ea28fd1aa7a86db6c02068d9111b6
Makulla <matthias.makulla@gmx.de>
2020-12-28 15:57:33 +0100

    test journal entry creation

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -5,11 +5,5 @@ Hooks.on("init", function() {
 });
 
 Hooks.on("ready", function() {
-    let d = Dialog.confirm({
-        title: "A Yes or No Question",
-        content: "<p>Choose wisely.</p>",
-        yes: () => console.log("You chose ... wisely"),
-        no: () => console.log("You chose ... poorly"),
-        defaultYes: false
-    });
+    let je = new JournalEntry({ name: "The boat" });
 });
